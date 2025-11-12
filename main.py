a37211c30fa71bbe0a5017ae784439a35c1c3a37
subh <subhgotbeans@gmail.com>
2025-11-12 10:35:34 +0000

    Your descriptive commit message

---
+++ main.py
@@ -0,0 +1,12 @@
+alu_bhaja = "frfrggggggggggggggggrgggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggfhhfgggggggfgggggggggggggggggggf"
+print(alu_bhaja)
+good = int(input("yo how much good you are on a scale of 1 to 10?"))
+result1 = good != 0
+if good >= 5:
+   print("good boi")
+elif good <= 11:
+   print("you liar")
+    
+else:
+   print("bad boi")
+   # i am lovin' it
